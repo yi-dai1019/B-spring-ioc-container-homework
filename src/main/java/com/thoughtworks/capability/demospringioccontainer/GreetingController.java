e3ead47f31b1b962b69e4a56302aa98a5fe4c742
yi-dai <yi.dai@thoughtworks.com>
2020-11-07 21:46:31 +0800

    Frist solution for question2

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,8 +1,11 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
 
 @RestController
 public class GreetingController {
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,8 +1,13 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
+
 @Service
+@Scope(value = SCOPE_PROTOTYPE, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class GreetingService {
 
     String sayHi() {
